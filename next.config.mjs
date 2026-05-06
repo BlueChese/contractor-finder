bd602bca1b624e5733917a26ced26ed55111eb3f
BlueChese <enuff.6574@gmail.com>
2026-05-05 22:07:37 -0500

    clean next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactCompiler: true,
-
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
 };
 
 export default nextConfig;
